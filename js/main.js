17b518493c26f6c726f9bf99a38cf8d22efb42c6
HarrynVega <99359773+HarrynVega@users.noreply.github.com>
2022-08-18 00:03:54 -0500

    Update: Updated the experience page
    Update: Updated CSS classes for experience page
    Update: Main.js to contain a Swiper.js carrousel function for the experience page
    Add: Added the images for the experience carrousel

--- js/main.js
+++ js/main.js
@@ -29,6 +29,28 @@ navToggle.addEventListener('click', () => {
     }
 })
 
+/* Experience Slider */
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false
+    },    
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
 /* Contact Site Send Email Form */
 
 function SendContact(){
